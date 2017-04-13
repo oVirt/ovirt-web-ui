41e659e021836049578a4a12adc9541642cfc17e
Marek Libra <mlibra@redhat.com>
2017-04-13 10:59:04 +0200

    Docker image can be built from latest release

--- tools/docker.buildAndPush.sh
+++ tools/docker.buildAndPush.sh
@@ -3,7 +3,9 @@
 ERROR_WRONG_ROOT_DIR=1
 ERROR_PARAMS=2
 
+GIT_REPO=oVirt/ovirt-web-ui
 GIT_URL=https://github.com/oVirt/ovirt-web-ui.git
+LATEST_RELEASE=https://github.com/oVirt/ovirt-web-ui/releases/latest
 DOCKER_BUILD_DIR=./dockerbuild
 
 DOCKER_BASE_TAG=ovirt-web-ui
@@ -13,7 +15,8 @@ DATE=`date +%Y_%m_%d`
 echo Build ovirt-web-ui docker image and push to Docker Hub
 
 function usage {
-  echo $0 master 
+  echo $0 master \# git clone recent master and build build ovirt-web-ui:master
+  echo $0 release \# download latest official release and build ovirt-web-ui:lastest
   echo $0 gittag [TAG]
   echo $0 pr [PULL_REQUEST_NUMBER] \# github pull-request to be applied on top of recent master
   echo $0 actual \# to build from recent sources
@@ -30,6 +33,29 @@ function checkRootDir {
     exit ${ERROR_WRONG_ROOT_DIR}
   fi
 }
+
+function getLatestReleaseName {
+  curl --silent "https://api.github.com/repos/${GIT_REPO}/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                                     # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                             # JSON value
+}
+
+function downloadLatestRelease {
+  RELNAME=`getLatestReleaseName`
+  LATEST=https://github.com/oVirt/ovirt-web-ui/archive/${RELNAME}.tar.gz
+  echo Latest release found: ${LATEST}
+  
+  clean
+  mkdir ${DOCKER_BUILD_DIR}
+  cd ${DOCKER_BUILD_DIR}
+
+  curl --silent -L ${LATEST} > ${RELNAME}.tar.gz
+  tar -xzf ${RELNAME}.tar.gz
+  cd ${DOCKER_BASE_TAG}-${RELNAME}
+
+  pwd # for debug only
+}
+
 
 function clone {
   clean
@@ -68,6 +94,13 @@ function actual {
   dockerBuild ${DOCKER_BASE_TAG}:devel_${DATE}
 }
 
+function release {
+  echo Build from latest release ${LATEST_RELEASE}
+
+  downloadLatestRelease
+  dockerBuild ${DOCKER_TAG}:latest
+}
+
 function gittag {
   echo Build from git tag: $1
 
@@ -92,6 +125,9 @@ checkRootDir
 case "$1" in
     master)
       master
+    ;;
+    release)
+      release
     ;;
     actual)
       actual
