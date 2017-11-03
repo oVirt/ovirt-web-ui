44ea8ffac82739b23886d36521c2104d91e97ae7
Jakub Niedermertl <jniederm@redhat.com>
2017-11-03 18:28:01 +0100

    intl: Empty translation not imported
    Empty translations from zanata are ignored for the app to be
    able to replace them by default strings.

--- scripts/intl/normalize-messages.js
+++ scripts/intl/normalize-messages.js
@@ -12,9 +12,28 @@ const DESTINATION = path.join('src', 'intl', 'translated-messages.json')
 function main() {
   const stringContent = fs.readFileSync(SOURCE, { encoding: 'utf8' })
   const parsedContent = JSON.parse(stringContent)
+  removeEmptyMessages(parsedContent)
   const serializedContent = stableStringify(parsedContent, { space: 2 }) + '\n'
   fs.writeFileSync(DESTINATION, serializedContent)
   console.log(chalk.green(`[normalize-messages.js] ${DESTINATION} written ✔`))
+}
+
+/**
+ * Sometimes Zanata returns empty string. These are removed for the app to be able to rendered the message in default
+ * language (English).
+ *
+ * @param translations object from zanata
+ */
+function removeEmptyMessages(translations) {
+  Object.keys(translations).forEach(langKey => {
+    const languageMessages = translations[langKey]
+    Object.keys(languageMessages).forEach(messageKey => {
+      const messageValue = languageMessages[messageKey]
+      if (messageValue === '') {
+        delete languageMessages[messageKey]
+      }
+    })
+  })
 }
 
 main()
