1486497c185cc705ec0a5174a086202f6ea093e7
Marek Libra <mlibra@redhat.com>
2017-11-06 12:48:36 +0100

    Decrease verbosity of ScrollPositionHistory

--- src/components/ScrollPositionHistory.js
+++ src/components/ScrollPositionHistory.js
@@ -2,14 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { loadFromSessionStorage, saveToSessionStorage } from '../storage'
-import { logDebug } from '../helpers'
 
 class ScrollPositionHistory extends React.Component {
   componentDidMount () {
     const { uniquePrefix } = this.props
 
     const scrollTop = loadFromSessionStorage(`${uniquePrefix}-scroll-top`) || 0
-    logDebug('ScrollPositionHistory.componentDidMount, restoring scrollTop: ', scrollTop)
 
     // So far, main browser's window is used for scrolling
     window.document.body.scrollTop = scrollTop
@@ -19,7 +17,6 @@ class ScrollPositionHistory extends React.Component {
     const { uniquePrefix } = this.props
 
     const scrollTop = window.document.body.scrollTop
-    logDebug('ScrollPositionHistory.componentWillUnmount, saving scrollTop: ', scrollTop)
     saveToSessionStorage(`${uniquePrefix}-scroll-top`, '' + scrollTop)
   }
 
