ab56614dbb1aff581509f4aabd274892eeddea37
Marek Libra <mlibra@redhat.com>
2017-11-09 09:08:32 +0100

    Add HTML IDs to VmsUserMessages

--- src/components/VmUserMessages/index.js
+++ src/components/VmUserMessages/index.js
@@ -11,12 +11,12 @@ import { clearUserMessages } from '../../actions/vm'
 import { hrefWithoutHistory } from '../../helpers'
 import { msg } from '../../intl'
 
-const UserMessage = ({ record }) => {
+const UserMessage = ({ record, id }) => {
   // TODO: render record.type
   return (
     <li className={`list-group-item ${style.crop}`} title={record.message} data-toggle='tooltip'>
       <span>
-        <pre className={style['message-box']}>
+        <pre className={style['message-box']} id={id}>
           <Time time={record.time} cssClass={style['usermsg-time']} />
           {record.message}
         </pre>
@@ -26,21 +26,23 @@ const UserMessage = ({ record }) => {
 }
 UserMessage.propTypes = {
   record: PropTypes.object.isRequired,
+  id: PropTypes.string,
 }
 
-const ContactAdminInfo = ({ userMessages }) => {
+const ContactAdminInfo = ({ userMessages, id }) => {
   if (userMessages.get('records').size === 0) {
     return null
   }
 
   return (
-    <div className={style['contact-admin']}>
+    <div className={style['contact-admin']} id={id}>
       Contact your administrator in case of issues
     </div>
   )
 }
 ContactAdminInfo.propTypes = {
   userMessages: PropTypes.object.isRequired,
+  id: PropTypes.string,
 }
 
 class VmUserMessages extends React.Component {
@@ -54,6 +56,7 @@ class VmUserMessages extends React.Component {
   render () {
     const { userMessages, onClearMessages } = this.props
 
+    const idPrefix = `usermsgs`
     const onToggle = () => {
       this.setState({ show: !this.state.show })
     }
@@ -63,25 +66,26 @@ class VmUserMessages extends React.Component {
       show = 'show'
     }
 
+    let idCounter = 0
     return (
       <li className='dropdown'>
-        <a className='dropdown-toggle nav-item-iconic' href='#' title={msg.messages()} onClick={hrefWithoutHistory(onToggle)}>
+        <a className='dropdown-toggle nav-item-iconic' href='#' title={msg.messages()} onClick={hrefWithoutHistory(onToggle)} id={`${idPrefix}-toggle`}>
           <i className='fa fa-bell' />
-          <span className='badge'>{userMessages.get('records').size}</span>
-          <span className='caret' />
+          <span className='badge' id={`${idPrefix}-size`}>{userMessages.get('records').size}</span>
+          <span className='caret' id={`${idPrefix}-caret`} />
         </a>
 
         <div className={`dropdown-menu dropdown-menu-right infotip bottom-right ${show}`}>
           <div className={`arrow ${style['fix-arrow-position']}`} />
 
           <ul className={`list-group ${style['messages-list']}`}>
-            {userMessages.get('records').map(r => (<UserMessage key={r.time} record={r} />))}
+            {userMessages.get('records').map(r => (<UserMessage key={r.time} record={r} id={`${idPrefix}-msg-${idCounter++}`} />))}
           </ul>
-          <ContactAdminInfo userMessages={userMessages} />
+          <ContactAdminInfo userMessages={userMessages} id={`${idPrefix}-contact`} />
 
           <div className='footer'>
-            <a href='#' onClick={hrefWithoutHistory(onClearMessages)}>{msg.clearMessages()}</a>
-            <a href='#' onClick={hrefWithoutHistory(onToggle)} className={style['close-button']}>{msg.close()}</a>
+            <a href='#' onClick={hrefWithoutHistory(onClearMessages)} id={`${idPrefix}-clear`}>{msg.clearMessages()}</a>
+            <a href='#' onClick={hrefWithoutHistory(onToggle)} className={style['close-button']} id={`${idPrefix}-close`}>{msg.close()}</a>
           </div>
         </div>
       </li>
