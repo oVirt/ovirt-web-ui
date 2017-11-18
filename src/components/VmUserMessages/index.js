723728508db4afa87a6fb977248b0abb3fc9e0a5
vladimir <vladimir@vladimir-K43SJ.(none)>
2017-11-18 16:06:11 +0100

    Remove number batch when no notifications

--- src/components/VmUserMessages/index.js
+++ src/components/VmUserMessages/index.js
@@ -67,11 +67,14 @@ class VmUserMessages extends React.Component {
     }
 
     let idCounter = 0
+    const badgeElement = userMessages.get('records').size === 0
+        ? null
+        : <span className='badge' id={`${idPrefix}-size`}>{userMessages.get('records').size}</span>
     return (
       <li className='dropdown'>
         <a className='dropdown-toggle nav-item-iconic' href='#' title={msg.messages()} onClick={hrefWithoutHistory(onToggle)} id={`${idPrefix}-toggle`}>
           <i className='fa fa-bell' />
-          <span className='badge' id={`${idPrefix}-size`}>{userMessages.get('records').size}</span>
+          {badgeElement}
           <span className='caret' id={`${idPrefix}-caret`} />
         </a>
 
