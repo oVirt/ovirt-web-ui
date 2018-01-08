0ff51e3b2c338c6c197bdcd488b78ebdd4f4be76
Bohdan Iakymets <biakymet@redhat.com>
2018-01-08 17:14:07 +0100

    Fixed bug with infinite scroll position after loading new VMs

--- src/components/Pages/VmsPage.js
+++ src/components/Pages/VmsPage.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import VmsList from '../VmsList/index'
 
-import style from './style.css'
-
 const VmsPage = () => {
-  return (<div className={`container-fluid ${style['vms-page']}`}>
-    <VmsList />
-  </div>)
+  return (<VmsList />)
 }
 
 export default VmsPage
