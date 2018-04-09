cc1f38de3eb7553edd2eec40fbac1f399e4f4103
Bohdan Iakymets <biakymet@redhat.com>
2018-04-09 15:04:40 +0200

    Fixed bug with delayed switch in Cloud-Init enable, also Hostname and SSH Keys typing delaying Bug: When click on Cloud-Init enable switch it has very long delay, problem was in function that putting as parameter in setState function. Fixes: https://github.com/oVirt/ovirt-web-ui/issues/546

--- src/components/VmDialog/index.js
+++ src/components/VmDialog/index.js
@@ -97,6 +97,8 @@ class VmDialog extends React.Component {
     this.onChangeVmCpu = this.onChangeVmCpu.bind(this)
     this.onChangeCD = this.onChangeCD.bind(this)
     this.onChangeBootMenuEnabled = this.onChangeBootMenuEnabled.bind(this)
+
+    this.handleCloudInitChange = this.handleCloudInitChange.bind(this)
   }
 
   componentWillMount () {
@@ -414,6 +416,15 @@ class VmDialog extends React.Component {
     this.setState(stateChange)
   }
 
+  handleCloudInitChange (key) {
+    return (value) => {
+      this.setState((prevState) => {
+        prevState.cloudInit[key] = value
+        return { cloudInit: prevState.cloudInit }
+      })
+    }
+  }
+
   render () {
     const { icons, vmDialog, clusters, templates, operatingSystems, storages, previousPath } = this.props
     const vm = this.props.vm
@@ -611,10 +622,9 @@ class VmDialog extends React.Component {
                 enabled={this.state.cloudInit.enabled}
                 hostName={this.state.cloudInit.hostName}
                 sshAuthorizedKeys={this.state.cloudInit.sshAuthorizedKeys}
-                onEnabledChange={value => this.setState(state => { state.cloudInit.enabled = value })}
-                onHostNameChange={value => this.setState(state => { state.cloudInit.hostName = value })}
-                onSshAuthorizedKeysChange={value =>
-                  this.setState(state => { state.cloudInit.sshAuthorizedKeys = value })}
+                onEnabledChange={this.handleCloudInitChange('enabled')}
+                onHostNameChange={this.handleCloudInitChange('hostName')}
+                onSshAuthorizedKeysChange={this.handleCloudInitChange('sshAuthorizedKeys')}
               />
 
             </dl>
