53d639697c77851db2164112cbd6185d774bcf62
Scott J Dickerson <sdickers@redhat.com>
2018-05-15 12:20:39 -0400

    Adopt react-router-navigation-prompt in VmDialog to replace react-router Prompt
     - App.js App component is stateless again
     - App.js App doesn't define react-router getUserConfirmation() anymore
     - VmDialog/index.js replace Prompt with NavigationPrompt
     - Use NavigationConfirmationModal directly within NavigationPrompt
     - have flow ignore the new dependency since we don't care about its type errors
    
    Using `NavigationPrompt`, every router page will need to define its own
    navigation confirmation handler.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { connect } from 'react-redux'
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 
-import VmsPageHeader from './components/VmsPageHeader/index'
-
-import Options from './components/Options'
 import AboutDialog from './components/About'
+import ContainerFluid from './components/ContainerFluid'
+import LoadingData from './components/LoadingData'
+import Options from './components/Options'
 import OptionsDialog from './components/OptionsDialog'
 import OvirtApiCheckFailed from './components/OvirtApiCheckFailed'
-import NavigationConfirmationModal from './components/NavigationConfirmationModal'
 import TokenExpired from './components/TokenExpired'
-import ContainerFluid from './components/ContainerFluid'
-import LoadingData from './components/LoadingData/index'
-
 import VerticalMenu from './components/VerticalMenu'
+import VmsPageHeader from './components/VmsPageHeader'
 
 import { getRoutes, getMenu } from './routes'
 import AppConfiguration from './config'
@@ -45,74 +40,40 @@ const NoLogin = () => {
  * Main App component. Wrap the main react-router components together with
  * the various dialogs and error messages that may be needed.
  */
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      navigationConfirm: {
-        show: false,
-        confirmYes: undefined,
-        confirmNo: undefined,
-      },
-    }
-  }
-
-  resetUserConfirmation (show = false, confirmYes = undefined, confirmNo = undefined) {
-    this.setState({
-      navigationConfirm: { show, confirmYes, confirmNo },
-    })
-  }
-
-  render () {
-    const { vms, visibility, config } = this.props
-
-    if (!config.get('loginToken')) { // login is missing
-      return (
-        <ContainerFluid>
-          <NoLogin />
-        </ContainerFluid>
-      )
-    }
-
-    let detailToRender = null
-    switch (visibility.get('dialogToShow')) {
-      case 'Options':
-        detailToRender = (<Options />)
-        break
-    }
-
-    const routes = getRoutes(vms)
-    const menu = getMenu()
-
-    const getUserConfirmation = (message, confirmCallback) => {
-      this.resetUserConfirmation(true, () => { confirmCallback(true) }, () => { confirmCallback(false) })
-    }
-    const userConfirmationYes = () => {
-      this.state.navigationConfirm.confirmYes()
-      this.resetUserConfirmation()
-    }
-    const userConfirmationNo = () => {
-      this.state.navigationConfirm.confirmNo()
-      this.resetUserConfirmation()
-    }
-
+const App = ({ vms, visibility, config }) => {
+  if (!config.get('loginToken')) { // login is missing
     return (
-      <Router getUserConfirmation={getUserConfirmation} basename={AppConfiguration.applicationURL}>
-        <div>
-          <VmsPageHeader page={vms.get('page')} title={fixedStrings.BRAND_NAME + ' ' + msg.vmPortal()} />
-          <VerticalMenu menuItems={menu} /> { /* Disabled, to enable search for left sidebar menu */ }
-          <LoadingData />
-          {renderRoutes(routes)}
-          {detailToRender}
-          <AboutDialog />
-          <OptionsDialog userId={config.getIn(['user', 'id'])} />
-          <OvirtApiCheckFailed />
-          <TokenExpired />
-          <NavigationConfirmationModal show={this.state.navigationConfirm.show} onYes={userConfirmationYes} onNo={userConfirmationNo} />
-        </div>
-      </Router>
+      <ContainerFluid>
+        <NoLogin />
+      </ContainerFluid>
     )
   }
+
+  let detailToRender = null
+  switch (visibility.get('dialogToShow')) {
+    case 'Options':
+      detailToRender = (<Options />)
+      break
+  }
+
+  const routes = getRoutes(vms)
+  const menu = getMenu()
+
+  return (
+    <Router basename={AppConfiguration.applicationURL}>
+      <div>
+        <VmsPageHeader page={vms.get('page')} title={fixedStrings.BRAND_NAME + ' ' + msg.vmPortal()} />
+        <VerticalMenu menuItems={menu} /> { /* Disabled, to enable search for left sidebar menu */ }
+        <LoadingData />
+        {renderRoutes(routes)}
+        {detailToRender}
+        <AboutDialog />
+        <OptionsDialog userId={config.getIn(['user', 'id'])} />
+        <OvirtApiCheckFailed />
+        <TokenExpired />
+      </div>
+    </Router>
+  )
 }
 App.propTypes = {
   vms: PropTypes.object.isRequired,
