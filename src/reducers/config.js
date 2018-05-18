e1c5543a60c22e94bbf71a699dbe6c5fb49fa8de
Scott J Dickerson <sdickers@redhat.com>
2018-05-18 15:02:20 -0400

    Refactor the scheduled data refresh to be able to stop and start
     - The scheduler can be started via `startSchedulerFixedDelay` with
       any delay (in seconds).
    
     - The scheduler can be stopped via `stopSchedulerFixedDelay` action.
       This will cancel the current `delay` and skip the task.
    
     - Starting a scheduler while another is already running will cause
       the existing scheduler to be stopped.

--- src/reducers/config.js
+++ src/reducers/config.js
@@ -1,5 +1,16 @@
 import Immutable from 'immutable'
 import { actionReducer } from './utils'
+import {
+  LOGIN_FAILED,
+  LOGIN_SUCCESSFUL,
+  LOGOUT,
+  SET_ADMINISTATOR,
+  SET_DOMAIN,
+  SET_OVIRT_API_VERSION,
+  SET_USB_FILTER,
+  SET_USER_FILTER_PERMISSION,
+  SHOW_TOKEN_EXPIRED_MSG,
+} from '../constants'
 
 const initialState = Immutable.fromJS({
   loginToken: undefined,
@@ -20,34 +31,34 @@ const initialState = Immutable.fromJS({
 })
 
 const config = actionReducer(initialState, {
-  LOGIN_SUCCESSFUL (state, { payload: { token, username, userId } }) {
+  [LOGIN_SUCCESSFUL] (state, { payload: { token, username, userId } }) {
     return state.merge({ loginToken: token, user: { name: username, id: userId } })
   },
-  LOGIN_FAILED (state) {
+  [LOGIN_FAILED] (state) {
     return state.delete('loginToken').deleteIn(['user', 'name'])
   },
-  LOGOUT (state) {
+  [LOGOUT] (state) {
     return state.delete('loginToken').deleteIn(['user', 'name'])
   },
-  SET_OVIRT_API_VERSION (state, { payload: { oVirtApiVersion } }) {
+  [SET_OVIRT_API_VERSION] (state, { payload: { oVirtApiVersion } }) {
     return state.merge({ oVirtApiVersion: oVirtApiVersion })
   },
-  SET_USER_FILTER_PERMISSION (state, { payload: { filter } }) {
+  [SET_USER_FILTER_PERMISSION] (state, { payload: { filter } }) {
     return state
       .set('filter', filter)
       .set('isFilterChecked', true)
   },
-  SET_ADMINISTATOR (state, { payload: { administrator } }) {
+  [SET_ADMINISTATOR] (state, { payload: { administrator } }) {
     return state.set('administrator', administrator)
   },
-  SHOW_TOKEN_EXPIRED_MSG (state) {
+  [SHOW_TOKEN_EXPIRED_MSG] (state) {
     return state
       .set('isTokenExpired', true)
   },
-  SET_DOMAIN (state, { payload: { domain } }) {
+  [SET_DOMAIN] (state, { payload: { domain } }) {
     return state.set('domain', domain)
   },
-  SET_USB_FILTER (state, { payload: { usbFilter } }) {
+  [SET_USB_FILTER] (state, { payload: { usbFilter } }) {
     return state.set('usbFilter', usbFilter)
   },
 }, true)
