ad5c654d23fc4a2d0d97974c47d4a35ba90c1650
Scott J Dickerson <sdickers@redhat.com>
2018-05-30 01:06:50 -0400

    Refactor App init and react-router Page rendering
     - `App` component will wait for login to complete before rendering
       routes. This will allow the route Pages to assume all relevant
       login data needed is available so additional API calls may be
       made.
    
     - Route titles (visible in the breadcrumbs) will show the requested
       VM or Pool UUID until the VM or Pool name is available from the
       store.
    
     - Each detail/edit route page will fetch the VM or Pool requested if
       it is not already available in the redux store.
    
     - Each detail/edit route page will sit with only the app header and
       breadcrumbs rendered until the VM or Pool is available in
       the redux store. The "Loading..." indicator should be present
       while fetching data.
    
     - Removed **isPool** prop from `VmDetail` since it can be derived
       from other props.

--- src/reducers/config.js
+++ src/reducers/config.js
@@ -33,16 +33,13 @@ const config = actionReducer(initialState, {
     return state.merge({ oVirtApiVersion: oVirtApiVersion })
   },
   SET_USER_FILTER_PERMISSION (state, { payload: { filter } }) {
-    return state
-      .set('filter', filter)
-      .set('isFilterChecked', true)
+    return state.set('filter', filter).set('isFilterChecked', true)
   },
   SET_ADMINISTATOR (state, { payload: { administrator } }) {
     return state.set('administrator', administrator)
   },
   SHOW_TOKEN_EXPIRED_MSG (state) {
-    return state
-      .set('isTokenExpired', true)
+    return state.set('isTokenExpired', true)
   },
   SET_DOMAIN (state, { payload: { domain } }) {
     return state.set('domain', domain)
