115c3a0eaddca164dfa3526262c6ff204b8c3135
Scott J Dickerson <sdickers@redhat.com>
2018-07-03 16:23:48 -0400

    ux-redesign: VM Details (editable card layout)
     - Use flex-box to layout the grid (created grid components to mimic
       bootstrap4 / patternfly4 style grids). These new grid components
       may be used in other places as well.
    
     - All of the cards use `BaseCard` to keep common layout
       and edit logic
    
     - Cards in this commit are placeholders. View and edit details to
       follow in later commits.

---
+++ src/components/VmDetails/GridComponents.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './style.css'
+
+/*
+ * Adapt flex-box based grid for the layout. It is much easier to work with
+ * than bootstrap3/patternfly grids.
+ */
+const Grid = ({ className = '', children, ...props }) => {
+  const cn = `${styles['grid-container']} ${className}`
+  return <div className={cn} {...props}>{children}</div>
+}
+Grid.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+}
+
+const Row = ({ className = '', children, ...props }) => {
+  const cn = `${styles['grid-row']} ${className}`
+  return <div className={cn} {...props}>{children}</div>
+}
+Row.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+}
+
+const Col = ({ className = '', cols = -1, style, children, content = 'expand', ...props }) => {
+  const cn = `${styles['grid-column']} ${className}`
+  const widthPercent = cols * 100 / 12
+  const col = cols <= 0 ? {} : {
+    flex: `0 0 ${widthPercent}%`,
+    maxWidth: `${widthPercent}%`,
+  }
+  return (
+    <div
+      column-content={content}
+      className={cn}
+      style={{ ...col, ...style }}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
+Col.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  cols: PropTypes.number,
+  style: PropTypes.object,
+  content: PropTypes.oneOf(['expand', 'auto']),
+}
+
+export {
+  Grid,
+  Row,
+  Col,
+}
