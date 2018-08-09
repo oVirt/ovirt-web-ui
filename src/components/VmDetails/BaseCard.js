246599236d851c59fc52e1675c92ad5bb0f8d019
Scott J Dickerson <sdickers@redhat.com>
2018-08-09 10:12:19 -0400

    ux-redesign: VM Details (editable card layout)
     - Use flex-box to layout the grid (created grid components to mimic
       bootstrap4 / patternfly4 style grids). These new grid components
       may be used in other places as well.
    
     - All of the cards use `BaseCard` to keep common layout
       and edit logic.
    
     - `BaseCard` edit state is controllable if needed and its event
       handlers can be aborted to prevent transition the edit state.
    
     - The page supports tracking the edit and dirty edit state of any
       component cards (via `onStartEdit` of `BaseCard` and `handleEditChange`
       of `VmDetailsContainer`).  Card are responsible to notify the
       layout page via the `onEditChange` callback about its **Edit** state
       and if any changes have been made on the Edit that need to be saved
       (**Dirty Edit**).
    
     - If any card reports **Dirty Edit**, the page will confirm prompt any
       navigation (or reload) request.
    
     - Cards in this commit are placeholders. View and edit details to
       follow in later commits.

---
+++ src/components/VmDetails/BaseCard.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Badge,
+  Button,
+  Card,
+  CardHeading,
+  CardTitle,
+  CardBody,
+  CardFooter,
+  Icon,
+  noop,
+  excludeKeys,
+} from 'patternfly-react'
+
+import style from './style.css'
+import CardEditButton from './CardEditButton'
+
+/**
+ * Base VM details card.  Support common layouts and view vs edit modes.
+ *
+ * Specifying __editMode__ allows the containing component to control the card's
+ * edit state.  Leave __editMode__ undefined to allow the card to control itself.
+ *
+ * When the user interacts with the card, the card will fire event handler:
+ *  - When in non-edit state, user clicks the __Edit__ icon -> onStartEdit() is called
+ *  - When in edit state, user clicks the __Cancel__ button -> onCancel() is called
+ *  - When in edit state, user clicks the __Save__ button -> onSave() is called
+ *
+ * If any of the event handlers return null, the card will not transition its edit state.
+ * This allows data validation or async operation completion.
+ */
+class BaseCard extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    icon: PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+    itemCount: PropTypes.number,
+
+    editMode: PropTypes.bool,
+    editable: PropTypes.bool,
+    editTooltip: PropTypes.string,
+
+    onStartEdit: PropTypes.func,
+    onCancel: PropTypes.func,
+    onSave: PropTypes.func,
+    children: PropTypes.func,
+  }
+  static defaultProps = {
+    onStartEdit: noop,
+    onCancel: noop,
+    onSave: noop,
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      edit: props.editMode,
+    }
+    this.propTypeKeys = Object.keys(BaseCard.propTypes)
+  }
+
+  clickEdit = () => {
+    if (this.props.onStartEdit() !== false) {
+      this.setState({ edit: true })
+    }
+  }
+
+  clickCancel = () => {
+    if (this.props.onCancel() !== false) {
+      this.setState({ edit: false })
+    }
+  }
+
+  clickSave = () => {
+    if (this.props.onSave() !== false) {
+      this.setState({ edit: false })
+    }
+  }
+
+  render () {
+    const {
+      title = undefined,
+      icon = undefined,
+      itemCount = undefined,
+      editMode = undefined,
+      editable = true,
+      editTooltip,
+      children = noop,
+    } = this.props
+    const editing = editMode === undefined ? this.state.edit : editMode
+    const hasHeading = !!title
+    const hasBadge = itemCount !== undefined
+    const hasIcon = icon && icon.type && icon.name
+
+    return (
+      <Card className={style['base-card']} {...excludeKeys(this.props, this.propTypeKeys)}>
+        {hasHeading && (
+          <CardHeading className={style['base-card-heading']}>
+            {editable && <CardEditButton tooltip={editTooltip} enabled={editing} onClick={this.clickEdit} />}
+            <CardTitle>
+              {hasIcon && <Icon type={icon.type} name={icon.name} className={style['base-card-title-icon']} />}
+              {title}
+              {hasBadge && <Badge className={style['base-card-item-count-badge']}>{itemCount}</Badge>}
+            </CardTitle>
+          </CardHeading>
+        )}
+
+        <CardBody className={style['base-card-body']}>
+          {(!hasHeading && editable) && (
+            <CardEditButton tooltip={editTooltip} enabled={editing} onClick={this.clickEdit} />
+          )}
+
+          {children({ isEditing: editing })}
+        </CardBody>
+
+        {editing && (
+          <CardFooter className={style['base-card-footer']}>
+            <Button bsStyle='primary' onClick={this.clickSave}><Icon type='fa' name='check' /></Button>
+            <Button onClick={this.clickCancel}><Icon type='pf' name='close' /></Button>
+          </CardFooter>
+        )}
+      </Card>
+    )
+  }
+}
+
+export default BaseCard
---
+++ src/components/VmDetails/index.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Alert, Card, CardBody } from 'patternfly-react'
+
+import styles from './style.css'
+
+import NavigationPrompt from 'react-router-navigation-prompt'
+import NavigationConfirmationModal from '../NavigationConfirmationModal'
+
+import { Grid, Row, Col } from './GridComponents'
+import DetailsCard from './cards/DetailsCard'
+import DisksCard from './cards/DisksCard'
+import NicsCard from './cards/NicsCard'
+import OverviewCard from './cards/OverviewCard'
+import SnapshotsCard from './cards/SnapshotsCard'
+import UtilizationCard from './cards/UtilizationCard'
+
+/**
+ * UX Redesign base component for the VM details card page.
+ *
+ * NOTE: I'm ignoring Pools and Pool VMs on purpose right now.
+ */
+class VmDetailsContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.trackEdits = { // changes to a card's edit state don't require a re-render here
+      edit: {},
+    }
+    this.state = {
+      anyDirtyEdit: false,
+    }
+  }
+
+  handleEditChange (card, isEdit, isDirty = false) {
+    const cardEdit = this.trackEdits.edit[card] || {}
+    cardEdit.edit = isEdit
+    cardEdit.dirty = isDirty
+
+    const edit = { ...this.trackEdits.edit, [card]: cardEdit }
+    const anyDirtyEdit = Object.entries(edit).reduce((acc, [card, value]) => acc || !!value.dirty, false)
+
+    console.debug(`card "${card}" \u2192 isEdit? ${isEdit}, isDirty? ${isDirty} ∴ edit`, edit, 'anyDirtyEdit?', anyDirtyEdit)
+    this.trackEdits = { edit, anyDirtyEdit }
+    if (!anyDirtyEdit !== !this.state.anyDirtyEdit) {
+      this.setState({ anyDirtyEdit })
+    }
+  }
+
+  render () {
+    const { vm } = this.props
+
+    return (
+      <Grid className={`route-page-component ${styles['details-container']}`}>
+        <NavigationPrompt when={this.state.anyDirtyEdit}>
+          {({ isActive, onConfirm, onCancel }) => (
+            <NavigationConfirmationModal show={isActive} onYes={onConfirm} onNo={onCancel} />
+          )}
+        </NavigationPrompt>
+
+        {vm.get('nextRunExists') &&
+          <Row>
+            <Col>
+              <Card>
+                <CardBody>
+                  <Alert type='info' style={{ margin: '0' }}>This VM has pending configurations changes that will be applied once the VM is shutdown (or rebooted).</Alert>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        }
+
+        <Row>
+          <Col cols={4}><OverviewCard vm={vm} onEditChange={(isEdit, isDirty) => this.handleEditChange('over', isEdit, isDirty)} /></Col>
+          <Col cols={5}><DetailsCard vm={vm} onEditChange={(isEdit, isDirty) => this.handleEditChange('detail', isEdit, isDirty)} /></Col>
+          <Col cols={3}><SnapshotsCard vm={vm} onEditChange={(isEdit, isDirty) => this.handleEditChange('snap', isEdit, isDirty)} /></Col>
+        </Row>
+        <Row>
+          <Col cols={9}><UtilizationCard vm={vm} onEditChange={(isEdit, isDirty) => this.handleEditChange('util', isEdit, isDirty)} /></Col>
+          <Col cols={3}>
+            <NicsCard vm={vm} onEditChange={(isEdit, isDirty) => this.handleEditChange('nic', isEdit, isDirty)} />
+            <DisksCard vm={vm} onEditChange={(isEdit, isDirty) => this.handleEditChange('disk', isEdit, isDirty)} />
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
+VmDetailsContainer.propTypes = {
+  vm: PropTypes.object,
+}
+
+export default VmDetailsContainer
