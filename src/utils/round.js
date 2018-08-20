38329d466c24c47a386c9768f602a9b34bb17fd7
Scott J Dickerson <sdickers@redhat.com>
2018-08-20 16:48:07 -0400

    utils/round.js - add rounding of arrays or object maps of numbers
    Round can now accept:
      - A number
      - A string parsable to a number
      - An array of numbers (or number strings)
      - An object map of numbers
      - Any nesting of arrays and object maps with numbers (or number strings)
    
    Implemented additional tests for various data structures that can be
    successfully rounded en mass.

--- src/utils/round.js
+++ src/utils/round.js
@@ -1,10 +1,25 @@
+import { isNumber } from './unit-conversion'
+
 /*
- * Round a number to the specified precision (negative precision is tens places,
- * 0 is to an integer, positive precision is to decimal places).
+ * Round a number, an array of numbers, or an Object map of numbers to the specified
+ * precision (negative precision is tens places, 0 is to an integer, positive precision
+ * is to decimal places).
  */
 export function round (number, precision = 0) {
-  const factor = Math.pow(10, precision)
-  const temp = number * factor
-  const roundedTemp = Math.round(temp)
-  return roundedTemp / factor
+  let rounded
+
+  if (isNumber(number)) {
+    const factor = Math.pow(10, precision)
+    const temp = number * factor
+    const roundedTemp = Math.round(temp)
+    rounded = roundedTemp / factor
+  } else if (Array.isArray(number)) {
+    rounded = number.map(n => round(n, precision))
+  } else if (number !== null && typeof number === 'object') {
+    rounded = Object.keys(number).reduce((acc, key) => { acc[key] = round(number[key], precision); return acc }, {})
+  } else {
+    throw new TypeError('number must be a number, an array of numbers or an object of numbers')
+  }
+
+  return rounded
 }
--- src/utils/round.test.js
+++ src/utils/round.test.js
@@ -27,3 +27,83 @@ describe('rounding precision tests', function () {
     expect(round(1.2345, 3)).toEqual(1.235)
   })
 })
+
+describe('round a number string', () => {
+  test('string \'5.67\' gets rounded to 5.6', () => {
+    expect(round('5.67', 1)).toEqual(5.7)
+  })
+})
+
+describe('rounding an array', function () {
+  it('to tens', function () {
+    expect(round([ 12344, 12345, 12346, 12354 ], -1)).toEqual([ 12340, 12350, 12350, 12350 ])
+  })
+
+  it('to integer', function () {
+    expect(round([ 123, 123.4, 123.5, 123.6 ], 0)).toEqual([ 123, 123, 124, 124 ])
+  })
+
+  it('to decimal', function () {
+    expect(round([ 1.23, 1.24, 1.25, 1.26 ], 1)).toEqual([ 1.2, 1.2, 1.3, 1.3 ])
+  })
+})
+
+describe('rounding an object', function () {
+  it('to tens', function () {
+    expect(round({ a: 12344, b: 12345, c: 12346, d: 12354 }, -1)).toEqual({ a: 12340, b: 12350, c: 12350, d: 12350 })
+  })
+
+  it('to integer', function () {
+    expect(round({ a: 123, b: 123.4, c: 123.5, d: 123.6 }, 0)).toEqual({ a: 123, b: 123, c: 124, d: 124 })
+  })
+
+  it('to decimal', function () {
+    expect(round({ a: 1.23, b: 1.24, c: 1.25, d: 1.26 }, 1)).toEqual({ a: 1.2, b: 1.2, c: 1.3, d: 1.3 })
+  })
+})
+
+describe('rounding an array of objects', function () {
+  it('to tens', function () {
+    expect(round([
+      { a: 12344, b: 12345 },
+      { c: 12346, d: 12354 },
+    ], -1))
+      .toEqual([
+        { a: 12340, b: 12350 },
+        { c: 12350, d: 12350 },
+      ])
+  })
+})
+
+describe('rounding an object of arrays of number', function () {
+  it('to decimal', function () {
+    expect(round({
+      a: [ 1.23, 1.24, 1.25 ],
+      b: 2.67,
+      c: [ 9.99, 8.76, 5.43 ],
+    }, 1))
+      .toEqual({
+        a: [ 1.2, 1.2, 1.3 ],
+        b: 2.7,
+        c: [ 10.0, 8.8, 5.4 ],
+      })
+  })
+})
+
+describe('fail on bad inputs', function () {
+  it('no arguments', function () {
+    expect(() => round()).toThrowError(TypeError)
+  })
+
+  it('single null', function () {
+    expect(() => round(null)).toThrowError(TypeError)
+  })
+
+  it('function input', function () {
+    expect(() => round(() => {})).toThrowError(TypeError)
+  })
+
+  it('string input', function () {
+    expect(() => round('ABC')).toThrowError(TypeError)
+  })
+})
