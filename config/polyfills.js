709b7630002038d1f9fa4419fa74b7e322b722cc
Bohdan Iakymets <biakymet@redhat.com>
2018-11-15 16:01:06 +0100

    Fixed IE11 support bug
    Added `babel-polyfill` and removed `promise` and `object-assign` packages.
    Fixes: https://github.com/oVirt/ovirt-web-ui/issues/869

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,19 +1,8 @@
 // NOTE: These polyfills are most likely unnecessary for our supported browsers. If verified,
 //       they may be removed and save the space in the bundle.
 
-// TODO: Verify if these polyfills are still necessary.
-
-if (typeof Promise === 'undefined') {
-  // Rejection tracking prevents a common issue where React gets into an
-  // inconsistent state due to an error, but it gets swallowed by a Promise,
-  // and the user has no idea what causes React's erratic future behavior.
-  require('promise/lib/rejection-tracking').enable()
-  window.Promise = require('promise/lib/es6-extensions.js')
-}
-
 // fetch() polyfill for making API calls.
 require('whatwg-fetch')
 
-// Object.assign() is commonly used with React.
-// It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign')
+// Babel-polyfills for using Array.prototype.includes, Object.assign and Promise
+require('babel-polyfill')
