77a40da2124f811e2dcb8d91bb061916fab5d994
Scott J Dickerson <sdickers@redhat.com>
2019-01-31 22:58:57 -0500

    Verify translated message formats match English
    Adds 2 new tests to verify the contents of `translated-messages.json`
    against the English source text in `messages.js`.
    
      1. Verify the translations only have keys that are already defined
         English text keys
    
      2. For messages that are ICU formatted messages, verify the
         translations have the same count and named arguments as the
         English message.
    
    A failure in test 1 will not prevent the app from functioning.
    
    A failure in test 2 will cause the locale with the failure to
    break at run time.  Typically this will only happen if (1) the
    translation has a typo in the argument name, or (2) if the argument
    name is changed in the English text but not carried forward to the
    translations.
    
    NOTE: If a fix the the translations is required to pass the test,
          the same change should be made in Zanata so it is retained
          in the next translation update.

---
+++ src/intl/messages.test.js
@@ -0,0 +1,73 @@
+/* eslint-env jest */
+
+import parser from 'intl-messageformat-parser'
+import { messages as englishMessages } from './messages'
+import translatedMessages from './translated-messages.json'
+
+/*
+ * Take a set of messages and generate a normalized object that can be deep compared
+ * to another to see if the message structures match.
+ *
+ * To support how messages are defined in `messages.js` and `translated-messages.json`,
+ * the __messages__ argument should be an object with keys of the format:
+ *
+ *   1. stringId: 'English string'
+ *   2. stringId: { message: 'English string', description: 'field description' }
+ *
+ * Regardless of input format, output will be normalized to look like:
+ *   {
+ *     '_id_': {
+ *       msg: '',
+ *       pattern: { type: 'argumentElement', id: ''argumentElement: '',  }
+ *   }
+ */
+function normalizeMessagesForDiff (messages) {
+  const normalForm = {}
+
+  // normalize the key and message from any of the valid source formats
+  Object.keys(messages).forEach(key => {
+    if (messages[key].message) {
+      normalForm[key] = { msg: messages[key].message }
+    } else if (typeof messages[key] === 'string') {
+      normalForm[key] = { msg: messages[key] }
+    }
+  })
+
+  // extract the format arguments from the messages
+  Object.keys(normalForm).forEach(name => {
+    const parsed = normalForm[name].msg ? parser.parse(normalForm[name].msg) : { type: 'empty' }
+    const args = normalForm[name].args = {}
+
+    if (parsed.type === 'messageFormatPattern') {
+      parsed.elements.forEach(element => {
+        if (element.type === 'argumentElement') {
+          args[element.id] = (element.format && element.format.type) || null
+        }
+      })
+    }
+  })
+
+  return normalForm
+}
+
+describe('verify the content of each locale in translated-messages.json', () => {
+  const englishNormalForm = normalizeMessagesForDiff(englishMessages)
+
+  Object.keys(translatedMessages).forEach(locale => {
+    describe(`check ${locale}`, () => {
+      const localeNormalForm = normalizeMessagesForDiff(translatedMessages[locale])
+
+      it('every locale key is a defined English key', () => {
+        Object.keys(localeNormalForm).forEach(name => {
+          expect(englishNormalForm).toHaveProperty(name)
+        })
+      })
+
+      it('messages match ICU arguments', function () {
+        Object.keys(localeNormalForm).forEach(name => {
+          expect(localeNormalForm[name].args).toEqual(englishNormalForm[name].args)
+        })
+      })
+    })
+  })
+})
