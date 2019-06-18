2f646a957ec2f216d0ee812955d317d3b897b4a0
Scott J Dickerson <sdickers@redhat.com>
2019-06-18 13:15:30 -0400

    Add `offset` to Col to allow layout positioning
    This change will allow `Col` tags to add left hand column padding
    to their layout.  An empty `Col` is no longer needed (or allowed).
    
    For example:
      <Grid>
        <Row>
          <Col offset={2} cols={4}> ... </Col>
          <Col cols={6}> ... </Col>
        </Row>
      </Grid>

--- src/components/Grid/GridComponents.js
+++ src/components/Grid/GridComponents.js
@@ -7,17 +7,18 @@ import styles from './style.css'
  * Adapt flex-box based grid for the layout. It is much easier to work with
  * than bootstrap3/patternfly grids.
  */
-const Grid = ({ className = '', children, style, ...props }) => {
+const Grid = ({ children, id, className = '', style }) => {
   const cn = `${styles['grid-container']} ${className}`
-  return <div className={cn} style={style}>{children}</div>
+  return <div className={cn} style={style} id={id}>{children}</div>
 }
 Grid.propTypes = {
   children: PropTypes.node.isRequired,
+  id: PropTypes.string,
   className: PropTypes.string,
   style: PropTypes.object,
 }
 
-const Row = ({ className = '', children, style, id }) => {
+const Row = ({ children, id, className = '', style }) => {
   const cn = `${styles['grid-row']} ${className}`
   return <div className={cn} style={style} id={id}>{children}</div>
 }
@@ -28,18 +29,27 @@ Row.propTypes = {
   style: PropTypes.object,
 }
 
-const Col = ({ className = '', cols = -1, style, children, content = 'expand', ...props }) => {
+const Col = ({ children, id, className = '', style, cols = -1, offset = -1, content = 'expand', ...props }) => {
   const cn = `${styles['grid-column']} ${className}`
+
+  if (cols === -1 && offset > 0) {
+    cols = 12 - offset
+  }
   const widthPercent = cols * 100 / 12
-  const col = cols <= 0 ? {} : {
+  const colStyle = cols <= 0 ? {} : {
     flex: `0 0 ${widthPercent}%`,
     maxWidth: `${widthPercent}%`,
   }
+  const offsetStyle = offset < 1 ? {} : {
+    marginLeft: `${offset * 100 / 12}%`,
+  }
+
   return (
     <div
       column-content={content}
       className={cn}
-      style={{ ...col, ...style }}
+      style={{ ...colStyle, ...offsetStyle, ...style }}
+      id={id}
       {...props}
     >
       {children}
@@ -47,10 +57,23 @@ const Col = ({ className = '', cols = -1, style, children, content = 'expand', .
   )
 }
 Col.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
+  id: PropTypes.string,
   className: PropTypes.string,
-  cols: PropTypes.number,
   style: PropTypes.object,
+  cols: function (props, propName, componentName) {
+    if (props.cols && (props.cols < 1 || props.cols > 12)) {
+      return new Error(`Prop 'cols' of component '${componentName}' is not in valid range of 1 .. 12 `)
+    }
+  },
+  offset: function (props, propName, componentName) {
+    if (props.offset !== undefined) {
+      const maxOffset = props.cols ? 12 - props.cols : 11
+      if (props.offset < 1 || props.offset > maxOffset) {
+        return new Error(`Prop 'offset' of component '${componentName}' is not in valid range of 1 .. ${maxOffset}`)
+      }
+    }
+  },
   content: PropTypes.oneOf(['expand', 'auto']),
 }
 
