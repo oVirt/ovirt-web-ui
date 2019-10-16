1f9bf6c995fceeea620dd6cebe464d8018bb1a52
Scott J Dickerson <sdickers@redhat.com>
2019-10-16 15:48:06 -0400

    Refactor suggestNicName out of NicsCard/index.js

--- src/components/utils/suggest-names.js
+++ src/components/utils/suggest-names.js
@@ -4,7 +4,7 @@ import { isNumber } from '_/utils'
  * Given a VM name and the VM's existing disks, suggest the name for a new Disk.
  *
  * @param {*} vmName VM's name
- * @param {*} existingDisks Array of the VM's existing disks
+ * @param {*} existingDisks Array of the VM's existing disks or disk names
  */
 export function suggestDiskName (vmName, existingDisks = []) {
   const regex = new RegExp(`^${vmName}_Disk(\\d+)$`)
@@ -20,4 +20,24 @@ export function suggestDiskName (vmName, existingDisks = []) {
 
   const nextDiskName = vmName + '_Disk' + (biggestNumber + 1)
   return nextDiskName
+}
+
+/*
+ * Search the VM's current NIC names and return the next unused numbered NIC name.
+ *
+ * @param {*} existingNics Array of the VM's existing nics or nic names
+ */
+export function suggestNicName (existingNics = []) {
+  const regex = new RegExp(`^nic(\\d+)$`)
+  let biggestNumber = 0
+
+  for (const nic of existingNics) {
+    const nicName = nic.name || nic
+    const [, number] = regex.exec(nicName) || []
+    if (isNumber(number)) {
+      biggestNumber = Math.max(biggestNumber, parseInt(number, 10))
+    }
+  }
+
+  return `nic${biggestNumber + 1}`
 }
