8b782fb49e03b4a5e774c64dd8107ffd69d5f50e
Hilda Stastna <hstastna@redhat.com>
2020-04-30 17:04:34 +0200

    Maximize Notification Drawer after clicking on toggle expand button
    Issue: https://github.com/oVirt/ovirt-web-ui/issues/1182
    
    Split VmUserMessages component in two and move Notification Drawer out of ul element
    to display it maximized and working properly, after clicking on the toggle expand button.

---
+++ src/components/VmUserMessages/Bellicon.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { hrefWithoutHistory } from '_/helpers'
+import { msg } from '_/intl'
+import OverlayTooltip from '../OverlayTooltip'
+
+const Bellicon = ({ userMessages, handleclick }) => {
+  const messagesCount = userMessages.get('records').size
+  const idPrefix = `usermsgs`
+  const badgeElement = messagesCount === 0
+    ? null
+    : <span className='badge' id={`${idPrefix}-size`}>{messagesCount}</span>
+
+  return (
+    <li>
+      <OverlayTooltip id={`${idPrefix}-tooltip`} tooltip={msg.notifications()} placement='bottom'>
+        <a className='dropdown-toggle nav-item-iconic' href='#' onClick={hrefWithoutHistory(handleclick)} id={`${idPrefix}-toggle`}>
+          <i className='fa fa-bell' />
+          {badgeElement}
+          <span className='caret' id={`${idPrefix}-caret`} />
+        </a>
+      </OverlayTooltip>
+    </li>
+  )
+}
+Bellicon.propTypes = {
+  handleclick: PropTypes.func.isRequired,
+  userMessages: PropTypes.object.isRequired,
+}
+
+export default connect(
+  (state) => ({
+    userMessages: state.userMessages,
+  })
+)(Bellicon)
