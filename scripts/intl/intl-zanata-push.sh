5814ec804fceb8b1d13f86d29b5f047c14cb7670
Scott J Dickerson <sdickers@redhat.com>
2020-10-12 10:31:06 -0400

    Update Zanata tools to use the maven plugin
    Since the zanata-cli rpm is old on F30 and may not exist in more
    recent Fedora or CentOS releases, the Zanata client used has been
    updated.  Now, the Zanata maven plugin is being used.  Java 1.8 is
    still a requirement to run the plugin, just like the zanata-cli.

--- scripts/intl/intl-zanata-push.sh
+++ scripts/intl/intl-zanata-push.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 [[ -e package.json ]] || cd ../..
 
-JAVA_HOME=$(alternatives --list | grep 'jre_1.8.0[[:space:]]' | cut -f 3)
+JAVA_HOME=$(alternatives --list | grep 'jre_1.8.0' | head -1 | cut -f 3)
+ZANATA_LOCALES=de,es,fr,it,ja,ko,pt-BR,zh-CN,cs
 
-zanata-cli push \
-    --errors \
-    --push-type source \
-    --locales de,es,fr,it,ja,ko,pt-BR,zh-CN,cs
+mvn \
+    org.zanata:zanata-maven-plugin:4.6.2:push \
+    -Dzanata.pushType="source" \
+    -Dzanata.locales="$ZANATA_LOCALES"
