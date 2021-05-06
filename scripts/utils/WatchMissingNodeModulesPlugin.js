164c8b4e5c2c78d0e6f934f2d0735a284fa3e756
Hilda Stastna <hstastna@redhat.com>
2021-05-06 10:37:22 +0200

    Fix error related to WatchMissingNodeModulesPlugin

--- scripts/utils/WatchMissingNodeModulesPlugin.js
+++ scripts/utils/WatchMissingNodeModulesPlugin.js
@@ -12,7 +12,7 @@ WatchMissingNodeModulesPlugin.prototype.apply = function (compiler) {
     var nodeModulesPath = this.nodeModulesPath;
 
     // If any missing files are expected to appear in node_modules...
-    if (missingDeps.some(file => file.indexOf(nodeModulesPath) !== -1)) {
+    if (Array.from(missingDeps).some(file => file.indexOf(nodeModulesPath) !== -1)) {
       // ...tell webpack to watch node_modules recursively until they appear.
       compilation.contextDependencies.push(nodeModulesPath);
     }
