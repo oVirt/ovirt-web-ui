702b68f89051cc35a3d4f8676109bd8da7f63cc2
Scott J Dickerson <sdickers@redhat.com>
2021-05-19 21:48:12 -0400

    Update jest configurations
    Follow the examples in create react app and updated the jest
    configs to get unit tests working along side the webpack upgrades.

---
+++ config/jest/babelTransform.js
@@ -0,0 +1,11 @@
+const babelJest = require('babel-jest')
+
+module.exports = babelJest.createTransformer({
+  babelrc: false,
+  configFile: false,
+
+  // This should match the paths.appSrc 'babel-loader' options.preset in webpack.config.dev.js
+  presets: [
+    [ 'react-app', { 'flow': true, 'typescript': false } ]
+  ],
+})
