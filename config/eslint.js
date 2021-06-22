2a05d03cf13abe6e7814713d91d6e23fbdcb8a50
Scott J Dickerson <sdickers@redhat.com>
2021-06-22 12:37:43 -0400

    Upgrade eslint, configs, plugins, parser
    Base changes:
    
      - Upgrade to eslint 7
    
      - Target ECMA version 11 / 2020 syntax.  This level is supported
        by the version of `@babel/preset-env`/`core-js` we are using.
    
      - Upgrade from babel-eslint to @babel/eslint-(parser|plugin)
    
      - Set the @babel/eslint-parser to use config file `babel.app.config.js`
    
      - Setup babel app source config to cache in `babel.app.config.js` so
        eslint will run.  This does not hurt the dev server or prod build.
    
    Updated some rules to follow our existing code conventions:
    
      - ternary multiline and indent
    
      - react/* only enable basic react and JSX handling and mostly warn
        instead of error
    
      - react/jsx-* for JSX formatting/indent/multiline are commented out

--- config/eslint.js
+++ config/eslint.js
@@ -1,124 +1,174 @@
-// Inspired by https://github.com/airbnb/javascript but less opinionated.
+// Based on https://standardjs.com/
+// Inspired by:
+//   - https://github.com/standard/eslint-config-standard
+//   - https://github.com/airbnb/javascript
+//   - https://github.com/facebook/create-react-app/blob/master/packages/eslint-config-react-app
+//   - https://github.com/openshift/console/tree/master/frontend/packages/eslint-plugin-console
 
-// We use eslint-loader so even warnings are very visibile.
-// This is why we only use "WARNING" level for potential errors,
-// and we don't use "ERROR" level at all.
-
-// In the future, we might create a separate list of rules for production.
-// It would probably be more strict.
+//
+// TODO 1: Integrate eslint-webpack-plugin to the webpack build so errors and warnings are
+//         visible during development.
+//
+// TODO 2: Adopt `eslint-plugin-jest` and `eslint-plugin-testing-library` for tests with
+//         an overrides section entry:
+//           https://github.com/facebook/create-react-app/blob/master/packages/eslint-config-react-app/jest.js
+//
+// TODO 3: Consider alternative or additional extends:
+//           eslint-config-airbnb, eslint-config-react-app, eslint-config-prettier
+//
 
 module.exports = {
   root: true,
 
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
 
-  extends: ['standard', 'standard-react'],
+  // include the `eslint-config-*` configs, plugins, settings, and rules from:
+  //   - https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json
+  //   - https://github.com/standard/eslint-config-standard-react/blob/master/eslintrc.json
+  extends: [
+    'standard',
+    'standard-react'
+  ],
+
+  plugins: [
+    '@babel',
+    'flowtype',
+    'import',
+    'react',
+    'react-hooks',
+  ],
 
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
+    es2020: true,
     jest: true,
     node: true,
   },
 
-  plugins: [
-    'flowtype',
-    'import',
-    'react-hooks',
-  ],
-
   parserOptions: {
-    ecmaVersion: 6,
+    // 2015=6, 2016=7, 2017=8, 2018=9, 2019=10, 2020=11, 2021=12
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
-      generators: true,
-      experimentalObjectRestSpread: true,
+    },
+    babelOptions: {
+      configFile: './config/babel.app.config.js',
     },
   },
 
   settings: {
-    'import/ignore': [
-      'node_modules',
-      '\\.(json|css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$',
-    ],
-    'import/extensions': ['.js'],
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.json'],
-      },
+    react: {
+      version: 'detect',
     },
-    'flowtype': {
-      'onlyFilesWithFlowAnnotation': true
-    }
+
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true,
+    },
   },
 
+  overrides: [
+    // this is where we can handle different files with different rules (jest, typescript)
+  ],
+
+  // override the rules we inherit from the `extends` modules
+  // rule refs:
+  //   - https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json
+  //   - https://github.com/standard/eslint-config-standard-react/blob/master/eslintrc.json
+  //   - https://github.com/facebook/create-react-app/blob/master/packages/eslint-config-react-app/base.js
+  //   - https://github.com/facebook/create-react-app/blob/master/packages/eslint-config-react-app/index.js
   rules: {
-    'no-duplicate-imports': 2,
-    'comma-dangle': [2, 'always-multiline'],
-    'object-curly-spacing': [2, 'always'],
-    'import/no-duplicates': 2,
+    'comma-dangle': [ 'error', {
+      "arrays": 'always-multiline',
+      "objects": 'always-multiline',
+      "imports": 'always-multiline',
+      "exports": 'always-multiline',
+      "functions": "never"
+    }],
+    'generator-star-spacing': [ 'error', { "before": false, "after": true } ],
+    'no-duplicate-imports': 'error',
+    'object-curly-spacing': [ 'error', 'always' ],
+    'prefer-object-spread': 'error',
+
+    // be picky about ternary multiline and indents
+    'multiline-ternary':  [ 'error', 'always-multiline' ],
+    // TODO: Consider using https://github.com/getify/eslint-plugin-proper-ternary
+
+    // modified from eslint-config-standard for ternary expressions
+    'indent': [
+      'error',
+      2,
+      {
+        "SwitchCase": 1,
+        "VariableDeclarator": 1,
+        "outerIIFEBody": 1,
+        "MemberExpression": 1,
+        "FunctionDeclaration": { "parameters": 1, "body": 1 },
+        "FunctionExpression": { "parameters": 1, "body": 1 },
+        "CallExpression": { "arguments": 1 },
+        "ArrayExpression": 1,
+        "ObjectExpression": 1,
+        "ImportDeclaration": 1,
+        "ignoreComments": false,
+        "ignoredNodes": ["TemplateLiteral *", "JSXElement", "JSXElement > *", "JSXAttribute", "JSXIdentifier", "JSXNamespacedName", "JSXMemberExpression", "JSXSpreadAttribute", "JSXExpressionContainer", "JSXOpeningElement", "JSXClosingElement", "JSXFragment", "JSXOpeningFragment", "JSXClosingFragment", "JSXText", "JSXEmptyExpression", "JSXSpreadChild"],
+
+        'flatTernaryExpressions': true,
+        'offsetTernaryExpressions': false,
+      },
+    ],
+
+    'import/no-duplicates': 'error',
+
+    // ref: https://github.com/yannickcr/eslint-plugin-react
+    // basic react and jsx rules
+    'react/no-unsafe': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-uses-react': 'warn',
+    // 'react/jsx-fragments': [ 'warn', 'syntax' ],
+    'react/jsx-equals-spacing': [ 'error', 'never' ],
+    'react/jsx-pascal-case': [ 'warn', { allowAllCaps: true, ignore: [] } ],
+
+    // JSX formatting, indents and multiline rules:
+    // 'react/jsx-closing-tag-location': 'warn',
+    // 'react/jsx-closing-bracket-location': [ 'warn', 'line-aligned' ],
+    // 'react/jsx-indent': [ 'warn', 2, { checkAttributes: true, indentLogicalExpressions: true } ],
+    // 'react/jsx-wrap-multilines': [
+    //   'warn',
+    //   {
+    //     "declaration": "parens",
+    //     "assignment": "parens",
+    //     "return": "parens",
+    //     "arrow": "parens",
+    //     "condition": "ignore",
+    //     "logical": "ignore",
+    //     "prop": "ignore"
+    //   },
+    // ],
+
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'flowtype/boolean-style': [
-      2,
-      'boolean'
-    ],
-    'flowtype/define-flow-type': 1,
-    'flowtype/delimiter-dangle': [
-      2,
-      'never'
-    ],
-    'flowtype/generic-spacing': [
-      2,
-      'never'
-    ],
-    'flowtype/no-primitive-constructor-types': 2,
-    'flowtype/no-types-missing-file-annotation': 2,
-    'flowtype/no-weak-types': 0,
-    'flowtype/object-type-delimiter': [
-      2,
-      'comma'
-    ],
-    'flowtype/require-parameter-type': [2, {
-      'excludeArrowFunctions': true
-    }],
-    'flowtype/require-return-type': [
-      2,
-      'always',
-      {
-        'excludeArrowFunctions': true,
-        'annotateUndefined': 'never'
-      }
-    ],
-    'flowtype/require-valid-file-annotation': 2,
-    'flowtype/semi': [
-      2,
-      'never'
-    ],
-    'flowtype/space-after-type-colon': [
-      2,
-      'always'
-    ],
-    'flowtype/space-before-generic-bracket': [
-      2,
-      'never'
-    ],
-    'flowtype/space-before-type-colon': [
-      2,
-      'never'
-    ],
-    'flowtype/type-id-match': [
-      2,
-      '^([A-Z][a-z0-9]+)+Type$'
-    ],
-    'flowtype/union-intersection-spacing': [
-      2,
-      'always'
-    ],
-    'flowtype/use-flow-type': 1,
-    'flowtype/valid-syntax': 1,
-    'generator-star-spacing': [2, {"before": false, "after": true}],
+
+    'flowtype/boolean-style': [ 'error', 'boolean' ],
+    'flowtype/define-flow-type': 'warn',
+    'flowtype/delimiter-dangle': [ 'error', 'never' ],
+    'flowtype/generic-spacing': [ 'error', 'never' ],
+    'flowtype/no-primitive-constructor-types': 'error',
+    'flowtype/no-types-missing-file-annotation': 'error',
+    'flowtype/no-weak-types': 'off',
+    'flowtype/object-type-delimiter': [ 'error', 'comma' ],
+    'flowtype/require-parameter-type': [ 'error', { 'excludeArrowFunctions': true } ],
+    'flowtype/require-return-type': [ 'error', 'always', { 'excludeArrowFunctions': true, 'annotateUndefined': 'never' } ],
+    'flowtype/require-valid-file-annotation': 'error',
+    'flowtype/semi': [ 'error', 'never' ],
+    'flowtype/space-after-type-colon': [ 'error', 'always' ],
+    'flowtype/space-before-generic-bracket': [ 'error', 'never' ],
+    'flowtype/space-before-type-colon': [ 'error', 'never' ],
+    'flowtype/type-id-match': [ 'error', '^([A-Z][a-z0-9]+)+Type$' ],
+    'flowtype/union-intersection-spacing': [ 'error', 'always' ],
+    'flowtype/use-flow-type': 'warn',
+    'flowtype/valid-syntax': 'warn',
+
+    // TODO: eslint-plugin-node rules for scripts/*?
   },
 }
