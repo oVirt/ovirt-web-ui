b17931a6098fdb2ef89b6ce3f3636b31ec492c11
Scott J Dickerson <sdickers@redhat.com>
2021-09-23 17:24:19 -0400

    Convert `activeRequests` reducer to ImmerJS

--- src/reducers/activeRequests.js
+++ src/reducers/activeRequests.js
@@ -1,18 +1,36 @@
-import { Set } from 'immutable'
+// @flow
 
-import { actionReducer } from './utils'
+import produce from 'immer'
 import { ADD_ACTIVE_REQUEST, REMOVE_ACTIVE_REQUEST } from '_/constants'
+import { actionReducer } from './utils'
 
-const initialState = Set()
+type ActiveRequestsStateType = Array<string>
+
+const initialState: ActiveRequestsStateType = []
+
+/*
+ * NOTE: immer has a pitfall where drafts are not referentially equal to the original
+ *       objects.  So any operation that relies on `==`, `===`, or `Object.is()` for
+ *       object references wont work without help.  To side-step that issue in this
+ *       reducer, the `requestId` is normalized to a JSON string.  String comparisons
+ *       with `==` and `===` are not subject to this pitfall.
+ *
+ * See: https://immerjs.github.io/immer/pitfalls#drafts-arent-referentially-equal
+ */
 
 const activeRequestsReducer = actionReducer(initialState, {
-  [ADD_ACTIVE_REQUEST] (state, { payload: requestId }) {
-    return state.add(requestId)
-  },
+  [ADD_ACTIVE_REQUEST]: produce((draft: ActiveRequestsStateType, { payload: requestId }) => {
+    const normalizedId = JSON.stringify(requestId)
+    if (draft.find(id => id === normalizedId) === undefined) {
+      draft.push(normalizedId)
+    }
+  }),
 
-  [REMOVE_ACTIVE_REQUEST] (state, { payload: requestId }) {
-    return state.delete(requestId)
-  },
+  [REMOVE_ACTIVE_REQUEST]: produce((draft: ActiveRequestsStateType, { payload: requestId }) => {
+    const normalizedId = JSON.stringify(requestId)
+    const d = draft.filter(id => id !== normalizedId)
+    return d
+  }),
 })
 
 export default activeRequestsReducer
