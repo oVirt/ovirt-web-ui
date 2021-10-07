656c7fa4b5faa02d63fd9b08a043b39dec108620
Scott J Dickerson <sdickers@redhat.com>
2021-10-07 12:24:19 -0400

    Refactor `activeRequests` actions/reducers around `uid`
    Updated transport `notifyStart()` to build a `requestTracker`
    that includes a unique id prefixed by "t_".  This gives us a single
    `uid` to track instead of keeping track of method/url.  Even multiple
    requests to the same method/url will each have a unique `uid` and allow
    each one to be tracked independently.

--- src/reducers/activeRequests.js
+++ src/reducers/activeRequests.js
@@ -4,6 +4,8 @@ import produce from 'immer'
 import { ADD_ACTIVE_REQUEST, REMOVE_ACTIVE_REQUEST } from '_/constants'
 import { actionReducer } from './utils'
 
+import type { RequestTrackerType } from '_/ovirtapi'
+
 type ActiveRequestsStateType = Array<string>
 
 const initialState: ActiveRequestsStateType = []
@@ -12,25 +14,27 @@ const initialState: ActiveRequestsStateType = []
  * NOTE: immer has a pitfall where drafts are not referentially equal to the original
  *       objects.  So any operation that relies on `==`, `===`, or `Object.is()` for
  *       object references wont work without help.  To side-step that issue in this
- *       reducer, the `requestId` is normalized to a JSON string.  String comparisons
- *       with `==` and `===` are not subject to this pitfall.
+ *       reducer, we rely on string comparison.  String comparisons with `==` and `===`
+ *       are not subject to this pitfall.
  *
  * See: https://immerjs.github.io/immer/pitfalls#drafts-arent-referentially-equal
  */
 
 const activeRequestsReducer = actionReducer(initialState, {
-  [ADD_ACTIVE_REQUEST]: produce((draft: ActiveRequestsStateType, { payload: requestId }) => {
-    const normalizedId = JSON.stringify(requestId)
-    if (draft.find(id => id === normalizedId) === undefined) {
-      draft.push(normalizedId)
+  [ADD_ACTIVE_REQUEST]: produce(
+    (draft: ActiveRequestsStateType, { payload: { method, url, uid } }: { payload: RequestTrackerType }) => {
+      if (draft.find(knownUid => knownUid === uid) === undefined) {
+        draft.push(uid)
+      }
     }
-  }),
+  ),
 
-  [REMOVE_ACTIVE_REQUEST]: produce((draft: ActiveRequestsStateType, { payload: requestId }) => {
-    const normalizedId = JSON.stringify(requestId)
-    const d = draft.filter(id => id !== normalizedId)
-    return d
-  }),
+  [REMOVE_ACTIVE_REQUEST]: produce(
+    (draft: ActiveRequestsStateType, { payload: { method, url, uid } }: { payload: RequestTrackerType }) => {
+      const d = draft.filter(knownUid => knownUid !== uid)
+      return d
+    }
+  ),
 })
 
 export default activeRequestsReducer
