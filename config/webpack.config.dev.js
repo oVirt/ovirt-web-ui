45e8356f8bedea393473040d7a26ad01cc5c665c
Jasper Berton <jasper.berton@team.blue>
2026-01-20 10:14:30 +0100

    packages: update webpack-dev-server to recent version
    To solve an issue with https://github.com/advisories/GHSA-9wv6-86v2-598j bump the webpack-dev-server.
    This only works with a higher node version so ensure presence in CI.
    
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -29,7 +29,7 @@ export default ({
     devServer: {
       host,
       port,
-      https,
+      server: https ? 'https' : 'http',
       historyApiFallback: {
         index: publicPath,
       },
